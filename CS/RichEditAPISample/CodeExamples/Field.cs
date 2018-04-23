b4fd97868839e416a652dfb5946e489ffad090c6
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RichEditAPISample/CodeExamples/Field.cs
@@ -0,0 +1,65 @@
+﻿using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichEditAPISample.CodeExamples
+{
+    class FieldActions
+    {
+        static void InsertField(Document document)
+        {
+            #region #InsertField
+            document.BeginUpdate();
+            document.Fields.Create(document.CaretPosition, "DATE");
+            document.Fields.Update();
+            document.EndUpdate();
+            #endregion #InsertField
+        }
+
+        static void ModifyFieldCode(Document document)
+        {
+            #region #ModifyFieldCode
+            document.BeginUpdate();
+            document.Fields.Create(document.CaretPosition, "DATE");
+            document.EndUpdate();
+            for (int i = 0; i < document.Fields.Count; i++)
+            {
+                string fieldCode = document.GetText(document.Fields[i].CodeRange);
+                if (fieldCode == "DATE")
+                {
+                    DocumentPosition position = document.Fields[i].CodeRange.End;
+                    document.InsertText(position, @"\@ ""M/d/yyyy h:mm am/pm""");
+                }
+            }
+            document.Fields.Update();
+            #endregion #ModifyFieldCode
+        }
+
+        static void CreateFieldFromRange(Document document)
+        {
+            #region #CreateFieldFromRange
+            document.BeginUpdate();
+            document.AppendText("SYMBOL 0x54 \\f Wingdings \\s 24");
+            document.EndUpdate();
+            document.Fields.Create(document.Paragraphs[0].Range);
+            document.Fields.Update();
+            #endregion #CreateFieldFromRange
+        }
+
+        static void ShowFieldCodes(Document document)
+        {
+            #region #ShowFieldCodes
+            document.LoadDocument("MailMergeSimple.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            for (int i = 0; i < document.Fields.Count; i++)
+            {
+                document.Fields[i].ShowCodes = true;
+            }
+            #endregion #ShowFieldCodes
+        }
+
+
+    }
+}
---
+++ CS/RichEditAPISample/CodeExamples/InlinePictures.cs
@@ -0,0 +1,55 @@
+﻿using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichEditAPISample.CodeExamples
+{
+    class InlinePicturesActions
+    {
+        static void ImageFromFile(Document document)
+        {
+            #region #ImageFromFile
+            DocumentPosition pos = document.Range.Start;
+            document.Images.Insert(pos, DocumentImageSource.FromFile("beverages.png"));
+            #endregion #ImageFromFile
+        }
+       
+        static void ImageCollection(Document document)
+        {
+            #region #ImageCollection
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            ReadOnlyDocumentImageCollection images = document.Images;
+            // If the width of an image exceeds 50 millimeters, 
+            // the image is scaled proportionally to half its size.
+            for (int i = 0; i < images.Count; i++)
+            {
+                if (images[i].Size.Width > DevExpress.Office.Utils.Units.MillimetersToDocumentsF(50))
+                {
+                    images[i].ScaleX /= 2;
+                    images[i].ScaleY /= 2;
+                }
+            }
+            #endregion #ImageCollection
+        }
+
+        static void SaveImageToFile(Document document)
+        {
+            #region #SaveImageToFile
+            document.LoadDocument("MovieRentals.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            DocumentRange myRange = document.CreateRange(0,100);
+            ReadOnlyDocumentImageCollection images = document.Images.Get(myRange);
+            if (images.Count > 0)
+            {
+                DevExpress.Office.Utils.OfficeImage myImage = images[0].Image;
+                System.Drawing.Image image = myImage.NativeImage;
+                string imageName = String.Format("Image_at_pos_{0}.png", images[0].Range.Start.ToInt());
+                image.Save(imageName);
+                System.Diagnostics.Process.Start("explorer.exe", "/select," + imageName);
+            }
+            #endregion #SaveImageToFile
+        }
+    }
+}
