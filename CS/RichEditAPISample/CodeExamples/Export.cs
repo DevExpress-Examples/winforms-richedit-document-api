b0efa1f7bb9e9d1c9ea8f7fac2ab365b4224d4c5
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RichEditAPISample/CodeExamples/Export.cs
@@ -0,0 +1,53 @@
+﻿using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichEditAPISample.CodeExamples
+{
+    class ExportActions
+    {
+
+        static void SaveImageFromRange(Document document)
+        {
+            #region #SaveImageFromRange
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            DocumentRange docRange = document.Paragraphs[2].Range;
+            ReadOnlyDocumentImageCollection docImageColl = document.Images.Get(docRange);
+            if (docImageColl.Count > 0)
+            {
+                DevExpress.Office.Utils.OfficeImage myImage = docImageColl[0].Image;
+                System.Drawing.Image image = myImage.NativeImage;
+                string imageName = String.Format("Image_at_pos_{0}.png", docRange.Start.ToInt());
+                image.Save(imageName);
+                System.Diagnostics.Process.Start("explorer.exe", "/select," + imageName);
+            }
+            #endregion #SaveImageFromRange
+        }
+
+        static void ExportRangeToHtml(Document document)
+        {
+            #region #ExportRangeToHtml
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            // Get the range for three paragraphs.
+            DocumentRange r = document.CreateRange(document.Paragraphs[0].Range.Start, document.Paragraphs[0].Range.Length + document.Paragraphs[1].Range.Length + document.Paragraphs[2].Range.Length);
+            // Export to HTML.
+            string htmlText = document.GetHtmlText(r, null);
+            System.IO.File.WriteAllText("test.html", htmlText);
+            // Show the result in a browser window.
+            System.Diagnostics.Process.Start("test.html");
+            #endregion #ExportRangeToHtml
+        }
+
+        static void ExportRangeToPlainText(Document document)
+        {
+            #region #ExportRangeToPlainText
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            string plainText = document.GetText(document.Paragraphs[2].Range);
+            System.Windows.Forms.MessageBox.Show(plainText);
+            #endregion #ExportRangeToPlainText
+        }
+    }
+}
---
+++ VB/RichEditAPISample/CodeExamples/Import.vb
@@ -0,0 +1,21 @@
+﻿Imports DevExpress.XtraRichEdit.API.Native
+Imports System
+Imports System.Collections.Generic
+Imports System.Linq
+Imports System.Text
+Imports System.Threading.Tasks
+
+Namespace RichEditAPISample.CodeExamples
+    Friend Class Import
+        Private Shared Sub ImportRtfText(ByVal document As Document)
+'            #Region "#ImportRtfText"
+            Dim rtfString As String = "{\rtf1\ansi\ansicpg1252\deff0\deflang1049" & ControlChars.CrLf & _
+"{\fonttbl{\f0\fswiss\fprq2\fcharset0 Arial;}" & ControlChars.CrLf & _
+"{\f1\fswiss\fcharset0 Arial;}}" & ControlChars.CrLf & _
+"{\colortbl ;\red0\green0\blue255;}" & ControlChars.CrLf & _
+"\viewkind4\uc1\pard\cf1\lang1033\b\f0\fs32 Test.\cf0\b0\f1\fs20\par}"
+            document.RtfText = rtfString
+'            #End Region ' #ImportRtfText
+        End Sub
+    End Class
+End Namespace
