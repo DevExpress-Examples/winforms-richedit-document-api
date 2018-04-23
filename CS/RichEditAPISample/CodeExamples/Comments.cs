10d23bc7563b33e3c6558fc777ed8fb0e9c22a96
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/RichEditAPISample/CodeExamples/Comments.cs
@@ -0,0 +1,77 @@
+﻿using DevExpress.XtraRichEdit.API.Native;
+using System;
+
+namespace RichEditAPISample.CodeExamples {
+    class CommentsActions    {
+
+        static void CreateComment(Document document)
+        {
+            #region #CreateComment
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            DocumentRange docRange = document.Paragraphs[2].Range;
+            string commentAuthor = "Johnson Alphonso D";
+            document.Comments.Create(docRange, commentAuthor, DateTime.Now);
+            #endregion #CreateComment
+        }
+
+        static void CreateNestedComment(Document document) {
+            #region #CreateNestedComment
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            if (document.Comments.Count > 0) {
+                DocumentRange[] resRanges = document.FindAll("trump", SearchOptions.None, document.Comments[1].Range);
+                if (resRanges.Length > 0) {
+                    Comment newComment = document.Comments.Create("Vicars Anny", document.Comments[1]);
+                    newComment.Date = DateTime.Now;
+                }
+            }
+            #endregion #CreateNestedComment
+        }
+
+        static void DeleteComment(Document document)
+        {
+            #region #DeleteComment
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            if (document.Comments.Count > 0)
+            {
+                document.Comments.Remove(document.Comments[0]);
+            }
+            #endregion #DeleteComment
+        }
+
+        static void EditCommentProperties(Document document)
+        {
+            #region #EditCommentProperties
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            int commentCount = document.Comments.Count;
+            if (commentCount > 0)
+            {
+                document.BeginUpdate();
+                DevExpress.XtraRichEdit.API.Native.Comment comment = document.Comments[document.Comments.Count - 1];
+                comment.Name = "New Name";
+                comment.Date = DateTime.Now;
+                comment.Author = "New Author";
+                document.EndUpdate();
+            }
+            #endregion #EditCommentProperties
+        }
+
+        static void EditCommentContent(Document document)
+        {
+            #region #EditCommentContent
+            document.LoadDocument("Grimm.docx", DevExpress.XtraRichEdit.DocumentFormat.OpenXml);
+            int commentCount = document.Comments.Count;
+            if (commentCount > 0)
+            {
+                DevExpress.XtraRichEdit.API.Native.Comment comment = document.Comments[document.Comments.Count - 1];
+                if (comment != null)
+                {
+                    SubDocument commentDocument = comment.BeginUpdate();
+                    commentDocument.InsertText(commentDocument.CreatePosition(0), "some text");
+                    commentDocument.Tables.Create(commentDocument.CreatePosition(9), 5, 4);
+                    comment.EndUpdate(commentDocument);
+                }
+            }
+            #endregion #EditCommentContent
+        }
+    }
+}
---
+++ CS/RichEditAPISample/CodeExamples/HeaderAndFooter.cs
@@ -0,0 +1,47 @@
+﻿using DevExpress.XtraRichEdit.API.Native;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RichEditAPISample.CodeExamples
+{
+    class HeaderAndFooterActions
+    {
+        static void CreateHeader(Document document)
+        {
+            #region #CreateHeader
+            Section firstSection = document.Sections[0];
+            // Create an empty header.
+            SubDocument newHeader = firstSection.BeginUpdateHeader();
+            firstSection.EndUpdateHeader(newHeader);
+            // Check whether the document already has a header (the same header for all pages).
+            if (firstSection.HasHeader(HeaderFooterType.Primary))
+            {
+                SubDocument headerDocument = firstSection.BeginUpdateHeader();
+                document.ChangeActiveDocument(headerDocument);
+                document.CaretPosition = headerDocument.CreatePosition(0);
+                firstSection.EndUpdateHeader(headerDocument);
+            }
+            #endregion #CreateHeader
+        }
+        
+        
+        static void ModifyHeader(Document document)
+        {
+            #region #ModifyHeader
+            document.AppendSection();
+            Section firstSection = document.Sections[0];
+            // Modify the header of the HeaderFooterType.First type.
+            SubDocument myHeader = firstSection.BeginUpdateHeader(HeaderFooterType.First);
+            DocumentRange range = myHeader.InsertText(myHeader.CreatePosition(0), " PAGE NUMBER ");
+            Field fld = myHeader.Fields.Create(range.End, "PAGE \\* ARABICDASH");
+            myHeader.Fields.Update();
+            firstSection.EndUpdateHeader(myHeader);
+            // Display the header of the HeaderFooterType.First type on the first page.
+            firstSection.DifferentFirstPage = true;
+            #endregion #ModifyHeader
+        }
+    }
+}
